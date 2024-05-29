478c405068c1e82f699d377876ce235898c381fe
Kris Brine <krisbrine@hotmail.com>
2024-05-29 11:26:21 -0300

    register

--- vin-decoder-client/src/Register.js
+++ vin-decoder-client/src/Register.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
 
     return (
-      <div>
-      <h1>Hey</h1>  
+      <div className='container'>
+        <h1>Register</h1>
+        <form>
+          <label>Email</label>
+          <input type="text"></input>
+          <label>Password</label>
+          <input type="password"></input>
 
+        </form>
       </div>
     );
 };
